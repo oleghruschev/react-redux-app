c44864837c766f5c84b68fc3bdbefe456538737c
Oleg <hruschev.oleg@mail.ru>
2018-10-24 21:35:14 +0500

    redux config

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,7 +1,6 @@
-import { Provider } from 'react-redux'
+
 import React, { Fragment }  from 'react';
-import { createStore } from 'redux'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 import Page1 from '../components/Page1';
@@ -11,17 +10,15 @@ import Navigation from '../components/Navigation';
 const history = createBrowserHistory();
 
 const Root = () => (
-  <Provider store={createStore(() => {}, {})}>
-    <BrowserRouter history={history}>
+  <Router history={history}>
+    <Fragment>
       <Navigation />
-      <Fragment>
-        <Switch>
-          <Route exact path='/' component={Page1} />
-          <Route path='/page2' component={Page2} />
-        </Switch>
-      </Fragment>
-    </BrowserRouter>
-  </Provider>
+      <Switch>
+        <Route exact path='/' component={Page1} />
+        <Route path='/page2' component={Page2} />
+      </Switch>
+    </Fragment>
+  </Router>
 );
 
 export default Root;
--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import { store } from './configureStore';
 import Root from './Root';
 
 ReactDOM.render(
-  <Root />,
+  <Provider store={store}>
+    <Root />
+  </Provider>,
   document.getElementById('app')
 )
 
